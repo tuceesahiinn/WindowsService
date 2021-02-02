7427831de7d69ef41ce8acf758835f44b3836159
Tugce Sahin <tuceesahiin@gmail.com>
2021-02-02 03:55:10 +0300

    end commit

---
+++ WindowsServiceDeneme/InnerOperation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsServiceDeneme
+{
+    public class InnerOperation
+    {
+        Timer timer = new Timer();
+        public void Start()
+        {
+            WriteToFile("Service başladı." + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 300000;
+            timer.Enabled = true;
+        }
+        public void Stop()
+        {
+            WriteToFile("Service Durduruldu"+DateTime.Now);
+        }
+
+        public void WriteToFile(string message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt"; ;
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+        }
+        private void OnElapsedTime(object source,ElapsedEventArgs e)
+        {
+            WriteToFile("Service Guncellendi " + DateTime.Now);
+        }
+    }
+}
---
+++ WindowsServiceDeneme/Service1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceDeneme
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        InnerOperation ınnerOperation = new InnerOperation();
+
+        protected override void OnStart(string[] args)
+        {
+            ınnerOperation.Start();
+        }
+
+        protected override void OnStop()
+        {
+            ınnerOperation.Stop();
+        }
+    }
+}
